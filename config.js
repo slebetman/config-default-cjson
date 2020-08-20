14d9613ece23c3d04f31ec55fcb26f0500d994c2
slebetman <slebetman@gmail.com>
2020-08-20 19:54:48 +0800

    Convert root path to string before resolving

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const cjson = require('cjson');
 const appRoot = require('app-root-path');
 
 function file (fname) {
-	return path.normalize(path.join(appRoot, fname));
+	return path.normalize(path.join(appRoot.toString(), fname));
 }
 
 var conf = cjson.load([
